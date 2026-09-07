f6640bd2ce00f2e72fef7ba5855160c3d626a50c
Michael <87988436+Mic-lab@users.noreply.github.com>
2026-09-07 14:22:03 -0400

    Removed some debug prints

--- data/scripts/game_states/game.py
+++ data/scripts/game_states/game.py
@@ -19,9 +19,9 @@ from ..map_system.level import Level, BossLevel
 class GameMap:
 
     LEVEL_NAMES = (
+            'bow_test',
             'pre_boss',
             'boss',
-            'bow_test',
 
             'tutorial_0',
             'tutorial_1',
@@ -62,7 +62,6 @@ class GameMap:
                 self.completed_transition = False
             self.level.update(game)
         else:
-            print(self.transition_timer, self.completed_transition)
             if self.transition_timer.ratio >= 0.5 and not self.completed_transition:
                 self.level_index += 1
                 self.load_level_from_index(game)
