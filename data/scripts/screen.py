6887bd40a1972fda358443ac5c8178469888ca8d
Michael <87988436+Mic-lab@users.noreply.github.com>
2026-09-13 01:56:35 -0400

    Set gl attributes for mac

--- data/scripts/screen.py
+++ data/scripts/screen.py
@@ -2,6 +2,12 @@ import pygame
 from . import config
 
 def create_screen():
+    # https://stackoverflow.com/a/76157313
+    pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MAJOR_VERSION, 3)
+    pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 3)
+    pygame.display.gl_set_attribute(pygame.GL_CONTEXT_PROFILE_MASK, pygame.GL_CONTEXT_PROFILE_CORE)
+    pygame.display.gl_set_attribute(pygame.GL_CONTEXT_FORWARD_COMPATIBLE_FLAG, True)
+
     return pygame.display.set_mode(config.screen_size,  pygame.OPENGL | pygame.DOUBLEBUF)
 
 
